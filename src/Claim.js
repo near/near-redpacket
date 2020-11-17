819336bca7b3045c0313fba40844458f60b244fb
Yan Zhu <picturepan2@hotmail.com>
2020-11-17 16:24:06 +0800

    Add English lang

--- src/Claim.js
+++ src/Claim.js
@@ -2,6 +2,7 @@ import 'regenerator-runtime/runtime';
 import React, { Component } from 'react';
 import * as nearApi from 'near-api-js';
 import { KeyPair } from 'near-api-js';
+import detectBrowserLanguage from 'detect-browser-language'
 import {
   nearTo, nearToInt, toNear, BOATLOAD_OF_GAS, DROP_GAS, NETWORK_ID, ACCESS_KEY_ALLOWANCE
 } from './util/near-util';
@@ -14,11 +15,16 @@ class Claim extends Component {
       status: true,
       secretkey: null,
       walletClaimUrl: '',
+      lang: '',
       amount: 0,
     }
   }
 
   async componentDidMount() {
+    // Detect browser lang
+    const detectBrowserLanguage = require('detect-browser-language')
+    this.setState({ lang: detectBrowserLanguage() })
+
     const {
       contractName,
       walletUrl
@@ -71,37 +77,37 @@ class Claim extends Component {
   }
 
   render() {
-    const { status, walletClaimUrl, amount } = this.state;
+    const { status, walletClaimUrl, amount, lang } = this.state;
     
     return (
       <div className="near-container">
         <div className="near-dapp near-dapp-redpacket">
           <div className="near-redpacket-header">
-            <img className="redpacket-cover" src={nearcover} alt="红包封面图" />
-            <button className="redpacket-btn">拆封</button>
+            <img className="redpacket-cover" src={nearcover} alt="" />
+            <button className="redpacket-btn">{this.state.lang == "zh-CN" ? "拆封" : "OPEN" }</button>
           </div>
           <div className="near-redpacket-body">
             <div className="redpacket-content">
-              <div className="redpacket-content-title">感谢您的关注</div>
-              <div className="redpacket-content-subtitle">来自 NEAR 团队的祝福</div>
+              <div className="redpacket-content-title">{this.state.lang == "zh-CN" ? "感谢您的关注" : "Thank You" }</div>
+              <div className="redpacket-content-subtitle">{this.state.lang == "zh-CN" ? "来自 NEAR 团队的祝福" : "FROM NEAR Protocol" }</div>
             </div>
             <div className="redpacket-card">
-              <img className="redpacket-cover" src={nearcover} alt="红包封面图" />
+              <img className="redpacket-cover" src={nearcover} alt="" />
               <div className="redpacket-card-header">
-                <div className="h2">感谢您的关注</div></div>
+                <div className="h2">{this.state.lang == "zh-CN" ? "感谢您的关注" : "Thank You" }</div></div>
               <div className="redpacket-card-body">
-                <div className="">金额</div>
+                <div className="">{this.state.lang == "zh-CN" ? "金额" : "AMOUNT" }</div>
                 { status ?
                   <div className="h1">{nearTo(amount, 2)}<small>Ⓝ</small></div>
                   :
-                  <div className="h2">已领取</div>
+                  <div className="h2">{this.state.lang == "zh-CN" ? "已领取" : "ALREADY CLAIMED" }</div>
                 }
               </div>
               <div className="redpacket-card-footer">
                 { status && walletClaimUrl ?
-                  <a className="btn btn-gold btn-block btn-lg" href={walletClaimUrl} target="_blank">注册并领取 NEAR</a>
+                  <a className="btn btn-gold btn-block btn-lg" href={walletClaimUrl} target="_blank">{this.state.lang == "zh-CN" ? "注册并领取 NEAR" : "CLAIM NEAR" }</a>
                   :
-                  <a className="btn btn-gold btn-block btn-lg disabled" href="#">无法领取 NEAR</a>
+                  <a className="btn btn-gold btn-block btn-lg disabled" href="#">{this.state.lang == "zh-CN" ? "无法领取 NEAR" : "Invalid Redpacket" }</a>
                 }
               </div>
             </div>
